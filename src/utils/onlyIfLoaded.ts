b40dd54897655d410a608cc69c1dbfb3f6838df1
Stuart Bourhill <stuburger@gmail.com>
2018-06-16 16:13:36 +0200

    fixed bug with bind guard function

--- src/utils/onlyIfLoaded.ts
+++ src/utils/onlyIfLoaded.ts
@@ -1,11 +1,10 @@
 import noop from './noop'
 
 export default function onlyIfLoaded(func: any, defaultFunc = noop) {
-  func = this.bind(func)
-  return this.bind(function(...params: any[]) {
+  return (...params: any[]) => {
     if (!this.props.disabled && !this.state.isBusy) {
       return func(...params)
     }
     return defaultFunc(...params)
-  })
+  }
 }
