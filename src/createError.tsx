0628067baffd4d3d3d2d2a18030b63107358e231
Stuart Bourhill <stuburger@gmail.com>
2018-06-26 17:22:20 +0200

    Added prop-types to Fault component Added invariant to Fault component to throw error when no path  is supplied

--- src/createError.tsx
+++ src/createError.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
+import PropTypes from 'prop-types'
 import { Path, FormProvider } from './sharedTypes'
+import invariant from 'invariant'
 
 export type InnerFaultProps = FormProvider<any> & ErrorProps
 
 export class InnerError extends React.Component<InnerFaultProps> {
   componentDidMount() {
     const { registerError, path, msg } = this.props
+    invariant(
+      path && path.length > 0,
+      "Invalid path. The 'path' prop on the Fault component is required when rendering a Fault " +
+        'outside of the component hierarchy of any Field, Section or Repeat components. ' +
+        'It is likely that you are seeing this message because you are ' +
+        'rendering a Fault as a direct child of your Form component.'
+    )
     registerError(path, msg)
   }
 
@@ -27,15 +36,16 @@ export interface ErrorProps {
 
 export function createFault(Consumer: React.Consumer<FormProvider<any, any>>) {
   return class Fault extends React.Component<ErrorProps> {
+    static propTypes = {
+      msg: PropTypes.string,
+      preventSubmit: PropTypes.bool,
+      path: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)])
+    }
     render() {
       const { preventSubmit, msg, path } = this.props
       return (
         <Consumer>
-          {props => (
-            <InnerError key={msg + preventSubmit + path} {...props} {...this.props}>
-              {this.props.children}
-            </InnerError>
-          )}
+          {props => <InnerError key={msg + preventSubmit + path} {...props} {...this.props} />}
         </Consumer>
       )
     }
