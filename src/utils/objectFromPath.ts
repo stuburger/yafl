9fd89ccd2abde047840489c46043e1cbdd39d8a1
Stuart Bourhill <stuburger@gmail.com>
2018-09-11 19:18:59 +0100

    attempt to reduce package size by removing lodash.set

--- src/utils/objectFromPath.ts
+++ src/utils/objectFromPath.ts
@@ -1,5 +1,5 @@
 import get from 'lodash.get'
-import set from 'lodash.set'
+import immutable from 'object-path-immutable'
 export default function<F extends object>(obj: F, paths: string[]): F {
-  return paths.reduce((ret, path) => set(ret, path, get(obj, path)), {} as F)
+  return paths.reduce((ret, path) => immutable.set(ret, path, get(obj, path)), {} as F)
 }
