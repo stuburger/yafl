356424d5b464c4fd9fba5df8895d989df4bbc310
sb <sb@trint.com>
2018-11-03 11:17:42 +0000

    changed copy

--- src/FieldSink.tsx
+++ src/FieldSink.tsx
@@ -5,7 +5,7 @@ const FieldSink: React.SFC<{ path: string }> = props => {
     false,
     `The <Field /> located at path '${
       props.path
-    }' rendered null. Make sure you supply one either the 'render', 'component' or 'type' prop.`
+    }' rendered null. Make sure you supply either the 'render', 'component' or 'type' prop.`
   )
   return null
 }
