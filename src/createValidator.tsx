752c9ddd6a3e80bc7f0505aea4cd706b95ed9453
sb <sb@trint.com>
2019-02-23 15:48:15 +0000

    use hooks in validator component

--- src/createValidator.tsx
+++ src/createValidator.tsx
@@ -3,15 +3,39 @@ import PropTypes from 'prop-types'
 import { Path, FormProvider } from './sharedTypes'
 import warning from 'tiny-warning'
 
-export type InnerValidatorProps<F extends object> = FormProvider<F> & {
+export type InnerValidatorProps = FormProvider<any> & {
   msg: string
 }
 
-export class InnerError<F extends object> extends React.Component<InnerValidatorProps<F>> {
-  componentDidMount() {
-    const { registerError, path, msg } = this.props
+function useWarning(condition: boolean, message: string) {
+  React.useEffect(() => {
+    warning(condition, message)
+  }, [condition, message])
+}
+
+export const InnerError: React.FC<InnerValidatorProps> = props => {
+  const { msg, path, registerError, unregisterError } = props
+  React.useEffect(() => {
+    registerError(path, msg)
+    return () => unregisterError(path, msg)
+  }, [])
+
+  return null
+}
+
+export interface ValidatorProps {
+  msg?: string | null | void
+  path: Path
+}
+
+export default function createValidator(context: React.Context<FormProvider<any>>) {
+  const Validator: React.FC<ValidatorProps> = props => {
+    const ctx = React.useContext(context)
+
+    const { path, msg, children = null } = props
+
     if (process.env.NODE_ENV !== 'production') {
-      warning(
+      useWarning(
         path && path.length > 0,
         "Invalid path. The 'path' prop on the Validator component is required when rendering a Validator " +
           'outside of the component hierarchy of any Field, Section or Repeat components. ' +
@@ -19,46 +43,20 @@ export class InnerError<F extends object> extends React.Component<InnerValidator
           'rendering a Validator as a direct child of your Form component.'
       )
     }
-    registerError(path, msg)
-  }
 
-  componentWillUnmount() {
-    const { unregisterError, path, msg } = this.props
-    unregisterError(path, msg)
-  }
-
-  render() {
-    return null
-  }
-}
-
-export interface ValidatorProps {
-  msg?: string | null | void
-  path?: Path | null
-}
-
-export default function createValidator<F extends object>(
-  context: React.Context<FormProvider<F, any>>
-): React.ComponentType<ValidatorProps> {
-  return class Validator extends React.PureComponent<ValidatorProps> {
-    context!: FormProvider<F, any>
-
-    static contextType = context
-    static propTypes /* remove-proptypes */ = {
-      msg: PropTypes.string,
-      path: PropTypes.arrayOf(
-        PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]).isRequired
-      )
+    if (typeof msg === 'string') {
+      return <InnerError key={msg + path} {...ctx} msg={msg} path={path} />
     }
 
-    render() {
-      const { msg, path, children } = this.props
-      const ctx = this.context
-      return typeof msg === 'string' ? (
-        <InnerError<F> key={msg + path} {...ctx} msg={msg} path={path || ctx.path} />
-      ) : (
-        children || null
-      )
-    }
+    return children as React.ReactElement<any>
   }
+
+  Validator.propTypes /* remove-proptypes */ = {
+    msg: PropTypes.string,
+    path: PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]).isRequired
+    ).isRequired
+  }
+
+  return Validator
 }
